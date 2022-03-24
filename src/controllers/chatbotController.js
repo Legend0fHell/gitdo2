78692daa785a7cdb956e588c956efcb0cc766ade
Loli-Laptop <pnquang05@gmail.com>
2022-03-24 23:51:30 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 let getHomePage = (req, res) => {
     return res.send("Hello")
 };
 
 let getWebhook = (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
     console.log(VERIFY_TOKEN);
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -14,7 +16,7 @@ let getWebhook = (req, res) => {
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
-        } 
+        }
         else {
             res.sendStatus(403);
         }
@@ -25,17 +27,35 @@ let postWebhook = (req, res) => {
     let body = req.body;
     if (body.object === 'page') {
         body.entry.forEach(function (entry) {
-            // event khi gửi tin nhắn, nhận ảnh, vv
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
         res.status(200).send('EVENT_RECEIVED');
-    } 
+    }
     else {
         res.sendStatus(404);
     }
 };
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
 module.exports = {
     getHomePage: getHomePage,
     getWebhook: getWebhook,
