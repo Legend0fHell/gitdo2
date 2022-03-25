ce6bd6bdfc9cffc0501c74364ec4a53998f3d39d
Loli-Laptop <pnquang05@gmail.com>
2022-03-25 08:58:31 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -4,6 +4,7 @@ import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
+let cache = {};
 let getHomePage = (req, res) => {
     return res.send("Hello")
 };
