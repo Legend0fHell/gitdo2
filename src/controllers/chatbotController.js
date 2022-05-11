98e8c6d988aa1600e1394501b49d2a977f5a9bf3
Loli-Laptop <pnquang05@gmail.com>
2022-05-11 13:41:01 +0700

    CLB update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -12,6 +12,7 @@ let getHomePage = (req, res) => {
 
 const CLBPostbackID = 'postback_card_626f69d246be3760af000038';
 const TKBPostbackID = 'postback_card_626f695446be37888700002d';
+const GetStartedPostbackID = 'GetStartedPostback';
 
 let getWebhook = (req, res) => {
     console.log(VERIFY_TOKEN);
@@ -136,6 +137,46 @@ function handlePostback(sender_psid, received_postback) {
     else if (payload.includes(CLBPostbackID)) {
         CLBPhase1(sender_psid, "Pg1");
     }
+    else if (payload.includes(GetStartedPostbackID)) {
+        HelloWorld(sender_psid);
+    }
+}
+
+function HelloWorld(sender_psid) {
+    let response;
+    response = {
+        "template_type": "generic",
+        "elements": [
+            {
+                "title": "Xin chào, mình là GitDo đây :<",
+                "image_url": "https://thumbs2.imgbox.com/4a/b5/0ZdluDVZ_t.png",
+                "subtitle": "Muốn hỏi gì hãy nói, buồn chuyện gì hãy kể nhé <3",
+                "default_action": {
+                    "type": "web_url",
+                    "url": "https://www.facebook.com/cybtechno/",
+                    "webview_height_ratio": "FULL"
+                },
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Thời khóa biểu",
+                        "payload": TKBPostbackID
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Lịch dạy thay",
+                        "payload": "postback_card_626f69a746be37888700002f"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Xem thêm!",
+                        "payload": "postback_card_626f6a4b46be372c1d000031"
+                    }
+                ]
+            }
+        ]
+    }
+    callSendAPI(sender_psid, response);
 }
 
 // Set the cache if the user request TKB.
