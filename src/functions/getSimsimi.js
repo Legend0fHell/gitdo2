ff21eac74878c4eff7dbeb9ab31c4c289439a47e
Minh <minhdangquang242006@gmail.com>
2022-06-02 23:39:59 +0700

    MINH

--- src/functions/getSimsimi.js
+++ src/functions/getSimsimi.js
@@ -1,9 +1,9 @@
 import { postMessenger, postGoogle, cache, getSimsimi } from '../controllers/chatbotController';
 
 // Set the cache if the user asked to get started.
-function Simsimi(sender_psid, text) {
+async function Simsimi(sender_psid, text) {
     if (sender_psid != '306816786589318') console.log('Simsimi: ', sender_psid);
-    let ans = getSimsimi(text);
+    let ans = await getSimsimi(text);
     console.log(ans);
     let response = { "text": text.success };
     postMessenger(sender_psid, response);
