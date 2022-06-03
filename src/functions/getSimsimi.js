effa7414c17d394d74613b6bc2d7b5ec173256cf
Loli-Laptop <pnquang05@gmail.com>
2022-06-03 11:23:09 +0700

    Quang

--- src/functions/getSimsimi.js
+++ src/functions/getSimsimi.js
@@ -12,7 +12,7 @@ async function Simsimi(sender_psid, text) {
 
     // Detect spam or emoji by counting the number of letter.
     let textDetect = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D').toUpperCase();
-    let resultDetect = /^[A-Z]+$/.test(textDetect)
+    let resultDetect = /[A-Z]/.test(textDetect)
     if(resultDetect == false) { // If not found any letter:
         console.log('Spam detected: ', sender_psid, " Detect: ", textDetect);
         // Random the response in the emoji array.
