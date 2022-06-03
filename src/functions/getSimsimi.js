d85dba97c3e2c49c79be3dd38701041d7a1734ca
Loli-Laptop <pnquang05@gmail.com>
2022-06-04 00:49:45 +0700

    Quang

--- src/functions/getSimsimi.js
+++ src/functions/getSimsimi.js
@@ -12,7 +12,7 @@ function valid (text) {
     const fs = require('fs');
     const star = "*************************************"
 
-    fs.readFile(path.join(__dirname, '..', 'data', 'vn_offensive_words.txt'), 'utf8', (err, data) => {
+    fs.readFile(require('path').join(__dirname, '..', 'data', 'vn_offensive_words.txt'), 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
