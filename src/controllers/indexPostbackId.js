3f04a4838d61b894cddaa829ac70a64b1b81b1ab
Loli-Laptop <pnquang05@gmail.com>
2022-06-04 22:56:16 +0700

    Changed PostbackID

--- src/controllers/indexPostbackId.js
+++ src/controllers/indexPostbackId.js
@@ -1,6 +1,6 @@
-export const CLB = "postback_card_626f69d246be3760af000038";
-export const TKB = "postback_card_626f695446be37888700002d";
+export const CLB = "CLBPostback";
+export const TKB = "TKBPostback";
 export const GetStarted = "GetStartedPostback";
-export const LDT = "postback_card_626f69a746be37888700002f";
-export const About = "postback_card_626f6a4b46be372c1d000031";
-export const HTHT = "postback_card_6277fdb046be37e05500002d";
+export const LDT = "LDTPostback";
+export const About = "AboutPostback";
+export const HTHT = "HTHTPostback";
