3890858f0da486209dc9f9d3724f6698b7ff61ac
Loli-Laptop <pnquang05@gmail.com>
2022-06-04 23:44:58 +0700

    Revamped command

---
+++ src/controllers/handleCommand.js
@@ -0,0 +1,52 @@
+import * as indexFunction from "../functions/indexFunction";
+import {cache} from "./chatbotController";
+
+export const handleCommand = (sender_psid, received_command) => {
+    if (sender_psid != "306816786589318") console.log("Received command: ", sender_psid, "Command: ", received_command);
+
+    // Split arguments by whitespace. Normalize the command arg.
+    const args = strNormalized.split(" ");
+    args[0] = args[0].toLowerCase();
+
+    // Normalize case by uppercase, de-Vietnamese.
+    let strNormalized = "";
+    try {
+        strNormalized = received_command.text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d").replace(/Đ/g, "D").toUpperCase();
+    } catch (error) {
+        return;
+    }
+
+    switch (args[0]) {
+    case "!tkb":
+        if (/\d/.test(strNormalized)) {
+            // If the line contains number, auto pass it to the GSheet to try it:
+            indexFunction.getTimetable.TKBPhase2(sender_psid, strNormalized);
+        } else {
+            // If the line doesn't contain number, if it was from phase1, incorrect input, else ask:
+            if (cache[sender_psid] === "TKB") {
+                indexFunction.getTimetable.TKBNotFound(sender_psid);
+            } else {
+                indexFunction.getTimetable.TKBPhase1(sender_psid);
+            }
+        }
+        break;
+    case "!clb":
+        indexFunction.getInfoClub.CLBPhase1(sender_psid, "Pg1");
+        break;
+    case "!restart":
+        indexFunction.postGetStarted.HelloWorld(sender_psid);
+        break;
+    case "!ldt":
+        indexFunction.getLDT.LDT(sender_psid);
+        break;
+    case "!about":
+        indexFunction.postAbout.about(sender_psid);
+        break;
+    case "!htht":
+        indexFunction.getSupport.HTHT(sender_psid);
+        break;
+    default:
+        // TODO: Add help since this is the "Not found command" case.
+        break;
+    }
+};
--- src/controllers/handleMessage.js
+++ src/controllers/handleMessage.js
@@ -1,7 +1,7 @@
 import * as indexFunction from "../functions/indexFunction";
+import {handleCommand} from "./handleCommand";
 import {cache} from "./chatbotController";
-
-const TKBPhrases = ["TKB", "THOIKHOABIEU", "MONGI", "HOCGI", "HOCJ"];
+import {handleCache} from "./handleCache";
 
 export const handleMessage = (sender_psid, received_message) => {
     // Don't analyze message from the bot itself.
@@ -10,28 +10,11 @@ export const handleMessage = (sender_psid, received_message) => {
     // Debugging line
     console.log("Received message: ", sender_psid, "Content: ", received_message.text);
 
-    // Normalize case by uppercase, trim whitespace, de-Vietnamese.
-    let strNormalized = "";
-    try {
-        strNormalized = received_message.text.replace(/ +/g, "").normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d").replace(/Đ/g, "D").toUpperCase();
-    } catch (error) {
-        return;
+    if (received_message.text.charAt(0) == "!") {
+        handleCommand(sender_psid, received_message.text);
+    } else if (cache[sender_psid]) {
+        handleCache(sender_psid, received_message.text, cache[sender_psid]);
+    } else {
+        indexFunction.getSimsimi.Simsimi(sender_psid, received_message.text);
     }
-
-    // Check if the line is saying about TKB:
-    if (cache[sender_psid] === "TKB" || TKBPhrases.some((v) => strNormalized.includes(v))) {
-        if (/\d/.test(strNormalized) || strNormalized.includes("DIU")) {
-            // If the line contains number, auto pass it to the GSheet to try it:
-            indexFunction.getTimetable.TKBPhase2(sender_psid, strNormalized);
-        } else {
-            // If the line doesn't contain number, if it was from phase1, incorrect input, else ask:
-            if (cache[sender_psid] === "TKB") {
-                indexFunction.getTimetable.TKBNotFound(sender_psid);
-            } else {
-                indexFunction.getTimetable.TKBPhase1(sender_psid);
-            }
-        }
-        return;
-    }
-    indexFunction.getSimsimi.Simsimi(sender_psid, received_message.text);
 };
