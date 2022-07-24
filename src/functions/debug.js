628e6f0047f60324748a12fb1acfabee93cbbb85
Loli-Laptop <pnquang05@gmail.com>
2022-07-24 16:36:45 +0700

    THPTQG

--- src/functions/debug.js
+++ src/functions/debug.js
@@ -1,11 +1,35 @@
 /* eslint-disable no-unused-vars */
-import {postMessenger, postGoogle, cache} from "../controllers/chatbotController";
-import {Firestore, FieldValue, Database, ServerValue} from "../controllers/handleFirestore";
-
+// import {postMessenger, postGoogle, cache} from "../controllers/chatbotController";
+// import {Firestore, FieldValue, Database, ServerValue} from "../controllers/handleFirestore";
+import request from "request";
+import UserAgent from "user-agents";
 // Debugging/Testing here (running locally instead of real testing on Heroku's servers.)
 // Need NodeJS initialized.
 // Run Debug by accessing localhost:8080.
 
 export const Debug = async () => {
-    console.log("debugging ok");
+    request({
+        uri: "https://diemthi.tuyensinh247.com/tsHighSchool/ajaxTracuuXephang",
+        method: "POST",
+        followAllRedirects: true,
+        headers: {"User-Agent": new UserAgent().toString()},
+    }, (err, res, body) => {
+        if (!err) {
+            // Database.ref("Telemetry/ExternalAPICall").child("TS247API").set(ServerValue.increment(1));
+            try {
+                const matches = body.match(/[\d\.]+/g);
+                console.log(matches[8], matches[10], matches[11]);
+            } catch (error) {
+                console.error("Unable to resolve JSON: " + body + "\nError: " + error + body);
+                // resolve("error");
+            }
+        } else {
+            console.error("Unable to POST: " + body + "\nError: " + err);
+            // resolve("error");
+        }
+    }).form({
+        vung: "CN",
+        block_code: "D00",
+        total_mark: "26.25",
+    });
 };
--- src/functions/getTHPTQG.js
+++ src/functions/getTHPTQG.js
@@ -1,4 +1,7 @@
 import {postMessenger} from "../controllers/chatbotController";
+import {Database, ServerValue} from "../controllers/handleFirestore";
+import request from "request";
+import UserAgent from "user-agents";
 
 function Help(sender_psid) {
     const response = {
@@ -26,11 +29,68 @@ async function THPTQG(sender_psid, text) {
         Help(sender_psid);
         return;
     }
-    console.log("THPTQG valid: ", sender_psid, "ID: ", textSplit[1]);
+    console.log("THPTQG valid: ", sender_psid, "ID: ", textSplit[2]);
+    if (textSplit[1] == "MB" || textSplit[1] == "MT" || textSplit[1] == "MN" || textSplit[1] == "CN") {
+        THPTRank(sender_psid, textSplit[1], textSplit[3], textSplit[2]);
+        return;
+    }
     const response = {
         "text": "Hiện tại tính năng đang được phát triển! Bạn quay lại sau nha!",
     };
     postMessenger(sender_psid, response);
+}
+const postTS247 = (vung, block_code, total_mark) => {
+    return new Promise((resolve) => {
+        request({
+            uri: "https://diemthi.tuyensinh247.com/tsHighSchool/ajaxTracuuXephang",
+            method: "POST",
+            followAllRedirects: true,
+            headers: {"User-Agent": new UserAgent().toString()},
+        }, (err, res, body) => {
+            if (!err) {
+                Database.ref("Telemetry/ExternalAPICall").child("TS247API").set(ServerValue.increment(1));
+                try {
+                    const matches = body.match(/[\d\.]+/g);
+                    resolve([matches[8], matches[10], matches[11]]);
+                } catch (error) {
+                    console.error("Unable to resolve JSON: " + body + "\nError: " + error + body);
+                    resolve(["error"]);
+                }
+            } else {
+                console.error("Unable to POST: " + body + "\nError: " + err);
+                resolve(["error"]);
+            }
+        }).form({
+            vung: vung,
+            block_code: block_code,
+            total_mark: total_mark,
+        });
+    });
+};
+
+async function THPTRank(sender_psid, vung, block_code, total_mark) {
+    const res = await postTS247(vung, block_code, total_mark);
+    if (res[0] == "error") {
+        Help(sender_psid);
+        return;
+    }
+    let vungViet;
+    if (vung == "MB") vungViet = "Miền Bắc";
+    if (vung == "MT") vungViet = "Miền Trung";
+    if (vung == "MN") vungViet = "Miền Nam";
+    if (vung == "CN") vungViet = "Toàn quốc";
+    postMessenger(sender_psid, {
+        "text": `
+Điểm bạn đang xét là: ${total_mark}. Khối ${block_code}. Khu vực: ${vungViet}.
+
+Số lượng thí sinh có điểm bằng ${total_mark} là: ${res[0]}
+Số lượng thí sinh có điểm hơn ${total_mark} là: ${res[1]}
+Số lượng thí sinh trong khối ${block_code} là: ${res[2]}
+
+Ghi chú: Xếp hạng chưa bao gồm điểm cộng, điểm ưu tiên. Số lượng HS trong khối là số HS thi đủ 3 môn nhưng có thể không xét tuyển bằng khối đó.
+From GitDo with love <3
+`,
+    });
 }
 
 export default {
