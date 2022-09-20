eb7f6c3230ef2ba2a0c9635a0655dd46af29d359
Loli-Laptop <pnquang05@gmail.com>
2022-09-20 22:05:38 +0700

    temp ldt

--- src/functions/getLDT.js
+++ src/functions/getLDT.js
@@ -3,7 +3,12 @@ import {postMessenger} from "../controllers/chatbotController";
 // Set the cache if the user asked to get started.
 function LDT(sender_psid) {
     if (sender_psid != "306816786589318") console.log("LDT: ", sender_psid);
-    const response = {"text": "Hiện tại chưa có lịch dạy thay mới nhaa!!"};
+    const response = {"attachment": {
+        "type": "image",
+        "payload": {
+            "attachment_id": "606230540960686",
+        },
+    }};
     postMessenger(sender_psid, response);
 }
 
