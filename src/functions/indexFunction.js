e9a5c2bf3c5e6f11832602d05ea185aab16e3ee2
Loli-Laptop <pnquang05@gmail.com>
2022-11-09 10:46:47 +0700

    Glitch Compliant

--- src/functions/indexFunction.js
+++ src/functions/indexFunction.js
@@ -1,18 +1,18 @@
-const getInfoClub = import("./getInfoClub.js").default;
-const getTimetable = import("./getTimetable.js").default;
-const postGetStarted = import("./postGetStarted.js").default;
-const getLDT = import("./getLDT.js").default;
-const postAbout = import("./postAbout.js").default;
-const getSimsimi = import("./getSimsimi.js").default;
-const getSupport = import("./getSupport.js").default;
-const getInfo = import("./getInfo.js").default;
-const postAdmin = import("./postAdmin.js").default;
-const getTS10 = import("./getTS10.js").default;
-const getTHPTQG = import("./getTHPTQG.js").default;
-const postOptinNoti = import("./postOptinNoti.js").default;
-const postHelp = import("./postHelp.js").default;
-const getTelemetryData = import("./getTelemetryData.js").default;
-const getSubTimetable = import("./getSubTimetable.js").default;
+import * as getInfoClub from "./getInfoClub.js";
+import * as getTimetable from "./getTimetable.js";
+import * as postGetStarted from "./postGetStarted.js";
+import * as getLDT from "./getLDT.js";
+import * as postAbout from "./postAbout.js";
+import * as getSimsimi from "./getSimsimi.js";
+import * as getSupport from "./getSupport.js";
+import * as getInfo from "./getInfo.js";
+import * as postAdmin from "./postAdmin.js";
+import * as getTS10 from "./getTS10.js";
+import * as getTHPTQG from "./getTHPTQG.js";
+import * as postOptinNoti from "./postOptinNoti.js";
+import * as postHelp from "./postHelp.js";
+import * as getTelemetryData from "./getTelemetryData.js";
+import * as getSubTimetable from "./getSubTimetable.js";
 
 export {
     getInfoClub, getTimetable, postGetStarted, getLDT, postAbout, getSimsimi, getSupport, getInfo, postAdmin, getTS10, postOptinNoti, postHelp, getTHPTQG, getTelemetryData, getSubTimetable,
